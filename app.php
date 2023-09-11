4955873fe0399d987f334c512d376f6b22925907
Aaron Klump <sourcecode@intheloftstudios.com>
2023-09-11 13:27:14 -0700

    fix autoload

--- app.php
+++ app.php
@@ -5,7 +5,11 @@ use AKlump\PhpSwap\Command\ExecuteCommand;
 use AKlump\PhpSwap\Command\ListCommand;
 use Symfony\Component\Console\Application;
 
-require __DIR__ . '/vendor/autoload.php';
+$autoload = __DIR__ . '/../../../vendor/autoload.php';
+if (!is_file($autoload)) {
+  $autoload = __DIR__ . '/vendor/autoload.php';
+}
+require_once $autoload;
 
 $app = new Application();
 $app->setName('phpswap');
