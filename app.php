f87bb5af2fdc2291b8c9318d56e51b5377836a88
Aaron Klump <sourcecode@intheloftstudios.com>
2023-10-09 13:40:25 -0700

    Bumped version number to 0.0.6

--- app.php
+++ app.php
@@ -13,7 +13,7 @@ require_once $autoload;
 
 $app = new Application();
 $app->setName('phpswap');
-$app->setVersion('0.0.5');
+$app->setVersion('0.0.6');
 $app->add(new ListCommand());
 $app->add(new ExecuteCommand());
 $app->run();
