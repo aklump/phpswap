e5cefe44264abcd2f5f23c2f95dfc24a15adbcc6
Aaron Klump <sourcecode@intheloftstudios.com>
2024-08-15 09:29:57 -0700

    Update knowledge binding format

--- knowledge/book.php
+++ knowledge/book.php
@@ -1,5 +1,7 @@
 <?php
 
+/** @var string $command */
+/** @var string $book_path */
 /** @var \Symfony\Component\EventDispatcher\EventDispatcher $dispatcher */
 
 use AKlump\Knowledge\Events\GetVariables;
@@ -8,5 +10,5 @@ use AKlump\PhpSwap\Execute;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $dispatcher->addListener(GetVariables::NAME, function (GetVariables $event) {
-  $event->addVariable('swapfile', Execute::SWAP_FILE);
+  $event->setVariable('swapfile', Execute::SWAP_FILE);
 });
