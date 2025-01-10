2e291193f0d8763548fcdf85a6d87859b5d53b88
Aaron Klump <sourcecode@intheloftstudios.com>
2025-01-10 10:51:13 -0800

    Bumped version number to 0.0.12

--- app.php
+++ app.php
@@ -13,7 +13,7 @@ require_once $autoload;
 
 $app = new Application();
 $app->setName('phpswap');
-$app->setVersion('0.0.11');
+$app->setVersion('0.0.12');
 $app->add(new ListCommand());
 $app->add(new ExecuteCommand());
 $app->run();
